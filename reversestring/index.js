c232ca55f7201fc4cfc623540eade4eb790dfe4d
rubimoon <dev.rubimoon@gmail.com>
2023-01-24 21:06:37 +0900

    add: solution 1
    making use of NodeJS native API

--- reversestring/index.js
+++ reversestring/index.js
@@ -5,7 +5,10 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+const s1 = require("./s1");
 
-function reverse(str) {}
+function reverse(str) {
+  return s1(str);
+}
 
 module.exports = reverse;
