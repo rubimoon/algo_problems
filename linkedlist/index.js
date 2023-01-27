d438f2112e7a521f9f59eff3e26da62698ebf668
rubimoon <dev.rubimoon@gmail.com>
2023-01-27 22:56:55 +0900

    add: insertFirst
    the head must know where the first node is

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -11,8 +11,15 @@ class Node {
 
 class LinkedList {
   constructor() {
-    // initialization
+    // the head store the first node
     this.head = null;
+  }
+
+  insertFirst(data) {
+    // the first node becomes the next node
+    const node = new Node(data, this.head);
+    // the list must know where the first node is in order to insert the first
+    this.head = node;
   }
 }
 
