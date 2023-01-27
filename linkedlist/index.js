3c45392a77c4cc0aa2f4897bb9f4246f8237503e
rubimoon <dev.rubimoon@gmail.com>
2023-01-27 22:17:08 +0900

    add: constructors by definition

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,18 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    // initialization
+    this.head = null;
+  }
+}
 
 module.exports = { Node, LinkedList };
