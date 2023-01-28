536916f31943b86f8d37a668c755f0f8c94d0f39
rubimoon <dev.rubimoon@gmail.com>
2023-01-28 15:49:52 +0900

    add: removeFirst method

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -51,6 +51,11 @@ class LinkedList {
   clear() {
     this.head = null;
   }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
 }
 
 module.exports = { Node, LinkedList };
