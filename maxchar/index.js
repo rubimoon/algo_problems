b97b367126470e542bc231019dae26264434b7ab
rubimoon <dev.rubimoon@gmail.com>
2023-02-02 21:26:35 +0900

    add: making use of for-in loop to get the key of the object

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let dic = {};
+  for (let char of str) {
+    if (!dic[char]) {
+      dic[char] = 1;
+    } else {
+      dic[char]++;
+    }
+  }
+
+  let max = 0;
+  let maxChar = "";
+  for (let char in dic) {
+    if (dic[char] > max) {
+      max = dic[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
