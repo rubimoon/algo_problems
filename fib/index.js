1a37bf87b3e7bff67aec3cb8a79eb4404a7cc185
rubimoon <dev.rubimoon@gmail.com>
2023-02-04 12:39:15 +0900

    add: first solution

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,23 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let f0 = 0;
+  let f1 = 1;
+  let fi = 0;
+
+  let fibArr = [];
+  for (let i = 0; i <= n; i++) {
+    if (i === 0) {
+      fi = f0;
+    } else if (i === 1) {
+      fi = f0 + f1;
+    } else {
+      fi = fi + fibArr[i - 2];
+    }
+    fibArr.push(fi);
+  }
+  return fi;
+}
 
 module.exports = fib;
