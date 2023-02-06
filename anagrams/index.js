c9a51c23194bf1499ca1ae720530ac0b9d386704
rubimoon <dev.rubimoon@gmail.com>
2023-02-06 20:41:49 +0900

    add: lame solution

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let objA = {};
+  let objB = {};
+  let isAnagram = true;
+
+  for (let c of stringA.toLowerCase()) {
+    if (c !== c.toUpperCase()) {
+      if (!objA[c]) {
+        objA[c] = 1;
+        continue;
+      }
+      objA[c]++;
+    }
+  }
+
+  for (let c of stringB.toLowerCase()) {
+    if (c !== c.toUpperCase()) {
+      if (!objB[c]) {
+        objB[c] = 1;
+        continue;
+      }
+      objB[c]++;
+    }
+  }
+
+  if (stringA.length >= stringB.length) {
+    for (const k in objA) {
+      isAnagram = objA[k] === objB[k];
+      if (!isAnagram) break;
+    }
+  } else {
+    for (const k in objB) {
+      isAnagram = objA[k] === objB[k];
+      if (!isAnagram) break;
+    }
+  }
+
+  return isAnagram;
+}
 
 module.exports = anagrams;
