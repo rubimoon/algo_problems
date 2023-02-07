1be6c3b7f04540e9ec4748d420ae320201f770db
rubimoon <dev.rubimoon@gmail.com>
2023-02-07 21:49:31 +0900

    add: weave method

--- weave/index.js
+++ weave/index.js
@@ -22,8 +22,17 @@
 //    q.remove() // 2
 //    q.remove() // 'There'
 
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  let weaved = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    weaved.add(sourceOne.remove());
+    weaved.add(sourceTwo.remove());
+  }
+
+  return weaved;
+}
 
 module.exports = weave;
